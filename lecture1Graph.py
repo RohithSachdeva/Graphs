2580fe4ae0e73afa9fd8fcc5a9d53b0747af9edf
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-10-07 14:44:07 -0700

    Need to add some MVP stuff to monday from another file; tuesday mvp done

--- lecture1Graph.py
+++ lecture1Graph.py
@@ -26,7 +26,30 @@ While the queue is not empty:
 
 Pop the queue into visited then have its adjacent nodes placed into the queue 
 
+
+DFT
+---
+Uses a stack (last in, first out) (nearest neighbors get put in the back of the line)
+Go to one neighbor ... add all their neighbors to the front of the queue... so basically one of the neighbors from your initial starting point keeps getting sent to the back of the queue 
+
+
 """
+
+
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
 class Queue():
     def __init__(self):
         self.queue = []
@@ -63,6 +86,9 @@ class Graph:
         else:
             raise IndexError("Nonexistent Vertex")
 
+    def get_neighbors(self, v):
+        return self.vertices[v]
+
     def bft(self, starting_vertex_id):
         q = Queue()
         visited = set()
@@ -78,7 +104,73 @@ class Graph:
             if v not in visited:
                 print(v) #Visit the node
 
-                visited.add(v)
+                visited.add(v) #set method to add 
+
+                for neighbor in self.get_neighbors(v):
+                    q.enqueue(neighbor)
+    
+    def dft(self):
+        q = Stack()
+        visited = set()
+        
+        #init
+        q.push(starting_vertex_id) #append the first starting vertex to the queue
+
+        #while queue isn't empty
+        while q.size() > 0:
+            
+            v = q.pop()
+
+            if v not in visited:
+                print(v) #Visit the node
+
+                visited.add(v) #set method to add 
+
+                for neighbor in self.get_neighbors(v):
+                    q.push(neighbor)
+    
+    def bfs(self, starting_vertex_id, target_vertex_id):
+        q = Queue()
+        visited = set()
+
+        q.enqueue([starting_vertex_id])
+
+        while q.size() > 0:
+            pass
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		# Create an empty queue and enqueue A PATH TO the starting vertex ID
+		# Create a Set to store visited vertices
+		# While the queue is not empty...
+			# Dequeue the first PATH
+			# Grab the last vertex from the PATH
+			# If that vertex has not been visited...
+				# CHECK IF IT'S THE TARGET
+				  # IF SO, RETURN PATH
+				# Mark it as visited...
+				# Then add A PATH TO its neighbors to the back of the queue
+				  # COPY THE PATH
+				  # APPEND THE NEIGHOR TO THE BACK
+
+
 
 
 
@@ -93,6 +185,9 @@ g.add_edge(2, 1)
 g.add_edge(1, 2)
 g.add_edge(2, 3)
 
+g.bft(2)
+
 #prints {1: {2}, 2: {1}}   1 connects to 2, 2 connects to 1 
 
 print(g.vertices) 
+
--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,5 +1,119 @@
 #BFT OR DFT CAN WORK;  Both will produce the same path cause graph is acyclic
 
+#Support hours; Using BFT 
+"""
+First step.. build a graph 
+We have all these different pairs of information 
+[(v1, v2),
+(v1, v2)]
+
+Earliest ancestor based on longest path
+"""
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = set()
+
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("Nonexistent Vertex")
+
+    def get_neighbors(self, vertex_id):
+        return self.vertices[vertex_id]
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    g = Graph()
+    
+    for pair in ancestors:
+        g.add_vertex(pair[0])
+        g.add_vertex(pair[1])
+        g.add_edge(pair[1], pair[0]) 
+        
+    
+    q = Queue()
+    q.enqueue([starting_node])
+    
+    oldest_ancestor = -1 #this is returned if node has no parents
+    max_path_length = 1
+
+    
+    while q.size() > 0: #while it exists; dequeue the path
+        
+        path = q.dequeue()
+        vertex = path[-1]
+
+        if len(path) >= max_path_length and vertex < oldest_ancestor:
+            oldest_ancestor = vertex
+            max_path_length = len(path)
+            
+        elif (len(path) > max_path_length):
+            max_path_length = len(path)
+            oldest_ancestor = vertex
+
+        for neighbor in g.vertices[vertex]:
+            path_copy = list(path)
+            path_copy.append(neighbor)
+            q.enqueue(path_copy)
+            
+    print(g.vertices)
+    return oldest_ancestor
+
+
+
+"""
+def earliest_ancestor(ancestors, starting_node):
+    # Build the graph
+    graph = Graph()
+    for pair in ancestors:
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
+        # Build edges in reverse
+        graph.add_edge(pair[1], pair[0])
+    # Do a BFS (storing the path)
+    q = Queue()
+    q.enqueue([starting_node])
+    max_path_len = 1
+    earliest_ancestor = -1
+    while q.size() > 0:
+        path = q.dequeue()
+        v = path[-1]
+        # If the path is longer or equal and the value is smaller, or if the path is longer)
+        if (len(path) >= max_path_len and v < earliest_ancestor) or (len(path) > max_path_len):
+            earliest_ancestor = v
+            max_path_len = len(path)
+        for neighbor in graph.vertices[v]:
+            path_copy = list(path)
+            path_copy.append(neighbor)
+            q.enqueue(path_copy)
+    return earliest_ancestor
+
+"""
+
+
+
+
+
+
+
+
+    
